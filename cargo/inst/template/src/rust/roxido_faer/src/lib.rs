7536cd6bf9c1fa0321dae683f004a87058c45046
David B. Dahl <dahl@stat.byu.edu>
2023-06-17 17:04:26 -0400

    Yes

--- cargo/inst/template/src/rust/roxido_faer/src/lib.rs
+++ cargo/inst/template/src/rust/roxido_faer/src/lib.rs
@@ -1,6 +1,5 @@
 use faer_core::MatRef;
-use roxido::rbindings::*;
-use roxido::Rval;
+use roxido::{Pc, Rval};
 
 pub trait RvalFaer {
     fn as_matrix_f64(self) -> Result<MatRef<'static, f64>, &'static str>;
@@ -9,6 +8,7 @@ pub trait RvalFaer {
 impl RvalFaer for Rval {
     fn as_matrix_f64(self) -> Result<MatRef<'static, f64>, &'static str> {
         Ok(unsafe {
+            use roxido::rbindings::*;
             if Rf_isMatrix(self.0) == 0 {
                 return Err("Not a matrix");
             }
@@ -20,5 +20,21 @@ impl RvalFaer for Rval {
             let slice = self.slice_double()?;
             MatRef::from_raw_parts(slice.as_ptr(), nrow, ncol, 1, nrow.try_into().unwrap())
         })
+    }
+}
+
+pub trait RvalMatRef {
+    fn to_rval(self, pc: &mut Pc) -> Rval;
+}
+
+impl RvalMatRef for MatRef<'_, f64> {
+    fn to_rval(self, pc: &mut Pc) -> Rval {
+        let nr = self.nrows();
+        let nc = self.ncols();
+        let (result_rval, result_slice) = Rval::new_matrix_double(nr, nc, pc);
+        for (k, r) in result_slice.iter_mut().enumerate() {
+            *r = self.read(k % nr, k / nc);
+        }
+        result_rval
     }
 }
