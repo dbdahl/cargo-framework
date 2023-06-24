27465cb80abf94dbce3ad9331f80bdfc8202ba7c
David B. Dahl <dahl@stat.byu.edu>
2023-06-23 22:56:41 -0600

    Cleaner

--- cargo/inst/template/src/rust/build.rs
+++ cargo/inst/template/src/rust/build.rs
@@ -3,25 +3,26 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    if let Some(out_dir) = env::var_os("OUT_DIR") {
-        let snippet = {
-            let src_path = Path::new("roxido.txt");
-            match env::var("R_PACKAGE_NAME") {
-                Err(_) => String::new(),
-                Ok(package_name) => match env::var("R_CARGO_SECOND_RUN") {
-                    Err(_) => {
-                        let _ = fs::remove_file(src_path);
-                        String::new()
-                    }
-                    Ok(_) => {
-                        let functions_info =
-                            fs::read_to_string(src_path).expect("Could not read file: {src_path}");
-                        let mut functions_info: Vec<_> = functions_info.lines().collect();
-                        functions_info.sort_unstable();
-                        functions_info.dedup();
-                        let mut snippet = String::new();
-                        snippet.push_str(&format!(
-                            r#"
+    println!("Running build.rs");
+    println!("cargo:rerun-if-changed=roxido.txt");
+    println!("cargo:rerun-if-env-changed=R_PACKAGE_NAME");
+    let out_dir = env::var_os("OUT_DIR").expect("OUT_DIR is not set");
+    let package_name = env::var("R_PACKAGE_NAME").unwrap_or_else(|_| String::new());
+    let src_path = Path::new("roxido.txt");
+    let snippet = match env::var("R_CARGO_SECOND_RUN") {
+        Err(_) => {
+            let _ = fs::remove_file(src_path);
+            String::new()
+        }
+        Ok(_) => match fs::read_to_string(src_path) {
+            Err(_) => String::new(),
+            Ok(functions_info) => {
+                let mut functions_info: Vec<_> = functions_info.lines().collect();
+                functions_info.sort_unstable();
+                functions_info.dedup();
+                let mut snippet = String::new();
+                snippet.push_str(&format!(
+                    r#"
                             use roxido::*;
 
                             #[no_mangle]
@@ -29,25 +30,25 @@ fn main() {
                                 let mut call_routines = Vec::with_capacity({});
                                 let mut _names: Vec<std::ffi::CString> = Vec::with_capacity({});
                             "#,
-                            package_name,
-                            functions_info.len(),
-                            functions_info.len()
-                        ));
-                        for line in functions_info {
-                            let tidbits: Vec<_> = line.split_whitespace().collect();
-                            snippet.push_str(&format!(
-                                r#"
+                    package_name,
+                    functions_info.len(),
+                    functions_info.len()
+                ));
+                for line in functions_info {
+                    let tidbits: Vec<_> = line.split_whitespace().collect();
+                    snippet.push_str(&format!(
+                        r#"
                                 _names.push(std::ffi::CString::new(".{}").unwrap());
                                 call_routines.push(rbindings::R_CallMethodDef {{
                                     name: _names.last().unwrap().as_ptr(),
                                     fun: unsafe {{ std::mem::transmute(crate::{} as *const u8) }},
                                     numArgs: {},
                                 }});"#,
-                                tidbits[0], tidbits[0], tidbits[1]
-                            ));
-                        }
-                        snippet.push_str(
-                            r#"
+                        tidbits[0], tidbits[0], tidbits[1]
+                    ));
+                }
+                snippet.push_str(
+                    r#"
                                 call_routines.push(rbindings::R_CallMethodDef {
                                     name: std::ptr::null(),
                                     fun: None,
@@ -66,13 +67,16 @@ fn main() {
                                 }
                                 roxido::r::set_custom_panic_hook();
                             }"#,
-                        );
-                        snippet
-                    }
-                },
+                );
+                snippet
             }
-        };
-        let dest_path = Path::new(&out_dir).join("registration.rs");
-        fs::write(&dest_path, snippet).expect("Could not write file: {dest_path}");
+        },
+    };
+    if snippet.is_empty() {
+        println!("Empty registration.rs");
+    } else {
+        println!("Nonempty registration.rs");
     }
+    let dest_path = Path::new(&out_dir).join("registration.rs");
+    fs::write(dest_path, snippet).expect("Could not write file: {dest_path}");
 }
